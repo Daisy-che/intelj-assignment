36dab58eca1680cdf88782b6d1bfdc0f501e708f
Daisy-che <daisychepkiruih@gmail.com>
2024-02-07 07:38:15 +0300

    added one file

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var fullName = "Daisy Chepkirui"
+    println(fullName)
+    var age =22
+    println(age)
+    var phoneNumber = +254748064111
+    println(phoneNumber)
+    var weight =67.3
+    println(weight)
+    var nationality = "Kenyan"
+    println(nationality)
+    val citizen = true
+    val nonCitizen = false
+    println(citizen)
+
+
+}
